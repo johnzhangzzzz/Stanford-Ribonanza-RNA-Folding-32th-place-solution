cbc002b315af61ed6d9748010a4d9785f72e70a9
johnzhangzzzz <154947981+johnzhangzzzz@users.noreply.github.com>
2024-07-02 12:34:38 +0800

    Add files via upload
    add .sh

---
+++ rna.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/johnzhangzzzz/Stanford-Ribonanza-RNA-Folding-32th-place-solution.git
+cd Stanford-Ribonanza-RNA-Folding-32th-place-solution
+pip install -r requirements.txt
+mkdir datamount
+cd datamount
+kaggle datasets download -d iafoss/stanford-ribonanza-rna-folding-converted
+unzip -n stanford-ribonanza-rna-folding-converted.zip
+rm stanford-ribonanza-rna-folding-converted.zip
+cd ..
+cd /workspace/Stanford-Ribonanza-RNA-Folding-32th-place-solution/repos/EternaFold/src
+make contrafold
+cd /workspace/Stanford-Ribonanza-RNA-Folding-32th-place-solution/
+python summary.py
+python train.py -C cfg_0
+python infer.py --Checkpoint ['datamount/weights/cfg_0/fold0/checkpoint_last_seed2023.pth']
